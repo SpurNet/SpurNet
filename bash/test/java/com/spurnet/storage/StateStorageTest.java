6d5cb5fba8a33a17fb914b8de6e4f48a0661fe1b
SpurNet <charlesphilipone@gmail.com>
2024-10-03 22:13:00 +0800

    Delete bash directory

--- bash/test/java/com/spurnet/storage/StateStorageTest.java
+++
@@ -1,8 +0,0 @@
-package com.spurnet.storage;
-
-import com.spurnet.model.State;
-
-public interface StateStorage {
-    void saveState(String stateKey, State state) throws Exception;
-    State getState(String stateKey) throws Exception;
-}
